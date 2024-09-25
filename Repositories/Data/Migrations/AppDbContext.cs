94368bb11d4887aa251a1e8587f7fe09d9995d39
nicole <devnicoleads@gmail.com>
2024-09-25 20:19:20 -0300

    Adicionado DbContext

---
+++ Repositories/Data/Migrations/AppDbContext.cs
@@ -0,0 +1,19 @@
+using ConsultaMedica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultaMedica.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Agendamento> Agendamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
